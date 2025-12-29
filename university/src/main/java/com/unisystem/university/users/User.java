ac72543c91448d0ab8f1552d0a27e63f6e32e7ce
mina maged <menamaged669@gmail.com>
2025-12-29 16:02:14 +0200

    user controller test

--- university/src/main/java/com/unisystem/university/users/User.java
+++ university/src/main/java/com/unisystem/university/users/User.java
@@ -55,6 +55,14 @@ public class User implements UserDetails{
         this.email = email;
         this.password = password;
     }
+
+    public User(Long id, String name, String email, String password, Role role){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
 }
 
 
--- university/src/test/java/com/unisystem/university/user/UserControllerTest.java
+++ university/src/test/java/com/unisystem/university/user/UserControllerTest.java
@@ -1,6 +1,9 @@
 package com.unisystem.university.user;
 
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
@@ -11,12 +14,19 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import com.unisystem.university.secutity.JwtUtils;
 import com.unisystem.university.secutity.SecurityConfig;
+import com.unisystem.university.users.Role;
 import com.unisystem.university.users.User;
 import com.unisystem.university.users.UserController;
 import com.unisystem.university.users.UserService;
@@ -50,12 +60,63 @@ public class UserControllerTest {
             }
             """;
 
-        // 2. Act & 3. Assert: Perform the POST and verify results
+        // Act & Assert: Perform the POST and verify results
         mockmvc.perform(post("/api/users/students")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(jsonRequest))
             .andExpect(status().isCreated())
             .andExpect(jsonPath("$.name").value("Mina"))
             .andExpect(jsonPath("$.email").value("Mina@gmail.com"));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void getLecturer_Test() throws Exception {
+        // Arrange 
+        User lecturer = new User("lec", "lec@gmail.com", "pass");
+        Long lecId = 1L;
+        lecturer.setId(lecId);
+        lecturer.setRole(Role.LECTURER);
+        when(userService.getLecturer(lecId)).thenReturn(lecturer);
+
+        // Act & Assert
+        mockmvc.perform(get("/api/users/lecturers/1"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.name").value("lec"));
+    }
+
+    @Test
+    @WithMockUser(roles = "SUPER_ADMIN")
+    public void findAdminsByName_Test() throws Exception {
+        // Arrange
+        User admin1 = new User(1L, "admin1", "admin1@gmail.com", "pass", Role.ADMIN);
+        User admin2 = new User(2L, "admin2", "admin2@gmail.com", "pass", Role.ADMIN);
+        List<User> admins = List.of(admin1, admin2);
+
+        String searchWord = "admin";
+        when(userService.findAdminsByName(searchWord)).thenReturn(admins);
+
+        // Act & Assert
+        mockmvc.perform(get("/api/users/adminsByName")
+                .param("searchWord", searchWord)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].name").value("admin1"))
+                .andExpect(jsonPath("$[1].name").value("admin2"));
+
+        verify(userService, times(1)).findAdminsByName(searchWord);
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void deleteStudent_Test() throws Exception {
+        // Arrange 
+        doNothing().when(userService).deleteStudent(1l);
+
+        mockmvc.perform(delete("/api/users/students/1"))
+            .andExpect(status().isNoContent());
+
+        verify(userService, times(1)).deleteStudent(1l);
     }
 }
